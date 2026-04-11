6799fb534cf154778361ee6ab3b3e0a6829a3369
sidjik <arsenii.milenchuk@student.tuke.sk>
2026-04-11 14:40:03 +0200

    FEAT: add function for process json with queries from phase A

--- services/rag/app/main.py
+++ services/rag/app/main.py
@@ -20,6 +20,9 @@ from app.core.utils import fetch_filepaths, save_json
 from app.crud.qdrant import create_collection, delete_collection
 from app.services.rag import ingest_document, query_documents
 
+import json
+import os
+
 
 async def process_dataset(
     input_dir: str | Path,
@@ -167,9 +170,101 @@ async def test2():
     await embedder.aclose()
 
 
+
+def reformat_chunks(chunks: list, query: dict)->dict:
+    result: dict = {
+        'documents': [],
+        'snippets': [],
+        **query
+    }
+    for chunk in chunks:
+        result['documents'].append(
+            f"http://www.ncbi.nlm.nih.gov/pubmed/{chunk['article']['pmid']}"
+        )
+        result['snippets'].append({
+            "beginSection": "abstract",
+            "endSection": "abstract",     
+            "text": chunk['article']['abstract'],
+            "document": f"http://www.ncbi.nlm.nih.gov/pubmed/{chunk['article']['pmid']}",
+            "offsetInBeginSection": 0,
+            "offsetInEndSection": len(chunk['article']['abstract']) 
+        })
+
+    return result
+        
+    
+
+
+
+async def process_json_from_bioasq(json_path: Path):
+    with open(json_path) as f:
+        query_data = json.load(f)
+
+    qdrant_url: str = os.environ.get("QDRANT_URL", "http://localhost:6333")
+    embed_url: str = os.environ.get("EMBED_URL", "http://localhost:8001")
+    reranker_url: str = os.environ.get("EMBED_URL", "http://localhost:8002")
+    vector_size: int = 1024 
+
+    qdrant = AsyncQdrantClient(url=qdrant_url)
+
+    embedder = httpx.AsyncClient(base_url=embed_url, timeout=30.0)
+    reranker = httpx.AsyncClient(base_url=reranker_url, timeout=60.0)
+
+    searched_results: dict[str, list] = {
+        "questions": []
+    }
+
+    for query in tqdm(query_data['questions'], desc="Processing queries ..."):
+
+        completed = False
+        while not completed:
+            try:
+                chunks = await query_documents(
+                    query['body'],
+                    "test-collection",
+                    qdrant=qdrant,
+                    embedder=embedder,
+                    reranker=reranker,
+                    with_sparse=True,
+                    fusion_alg="dbsf",
+                    search_top_k=20,
+                    rerank_top_k=5,
+                    batch_size=10,
+                    log_dir=RESULTS_DIR / "hybrid",
+                )
+
+                # filter chunks by tresh
+                chunks = list(filter(
+                    lambda x: x['score'] >= 0.7,
+                    chunks
+                ))
+
+                searched_results['questions'].append(
+                    reformat_chunks(chunks, query)
+                )
+                completed = True
+            except Exception:
+                continue
+
+
+
+    await qdrant.close()
+    await embedder.aclose()
+    await reranker.aclose()
+
+    with open(json_path.parent / "processed_query_for_rag.json", 'w') as f:
+        json.dump(searched_results, f, ensure_ascii=False, indent=2)
+
+
+
+
+
 async def main():
     # await test1()
-    await test2()
+    #await test2()
+    await process_json_from_bioasq(
+        json_path=Path(__file__).parents[1] / "query_for_rag.json"
+    )
 
 
 if __name__ == "__main__":
